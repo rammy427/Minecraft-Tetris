570fd6c20febe9540d97b0ba4465c87298d7ed27
GamingRex4274 <grex4274@gmail.com>
2021-06-24 15:09:50 -0400

    Added support for bevel frame drawing.

--- Engine/Beveler.cpp
+++ Engine/Beveler.cpp
@@ -25,10 +25,40 @@ void Beveler::DrawOuterBevel(const RectI& rect, int size, Graphics& gfx) const
 	gfx.DrawIsoRightTriBL(right - size, bottom - size, size, bottomColor);
 }
 
+void Beveler::DrawInnerBevel(const RectI& rect, int size, Graphics& gfx) const
+{
+	const int left = int(rect.left);
+	const int right = int(rect.right);
+	const int top = int(rect.top);
+	const int bottom = int(rect.bottom);
+	// left side bevel (corner,side,corner)
+	gfx.DrawIsoRightTriBL(left, top, size, rightColor);
+	gfx.DrawRect(left, top + size, left + size, bottom - size, rightColor);
+	gfx.DrawIsoRightTriUL(left, bottom - size, size, rightColor);
+	// top side bevel
+	gfx.DrawIsoRightTriUR(left, top, size, bottomColor);
+	gfx.DrawRect(left + size, top, right - size, top + size, bottomColor);
+	gfx.DrawIsoRightTriUL(right - size, top, size, bottomColor);
+	// right side bevel
+	gfx.DrawIsoRightTriBR(right - size, top, size, leftColor);
+	gfx.DrawRect(right - size, top + size, right, bottom - size, leftColor);
+	gfx.DrawIsoRightTriUR(right - size, bottom - size, size, leftColor);
+	// bottom side bevel
+	gfx.DrawIsoRightTriBR(left, bottom - size, size, topColor);
+	gfx.DrawRect(left + size, bottom - size, right - size, bottom, topColor);
+	gfx.DrawIsoRightTriBL(right - size, bottom - size, size, topColor);
+}
+
 void Beveler::DrawBeveledBrick(const RectI& rect, int bevelSize, Graphics& gfx) const
 {
 	DrawOuterBevel(rect, bevelSize, gfx);
 	gfx.DrawRect(rect.GetExpanded(-bevelSize), baseColor);
+}
+
+void Beveler::DrawBevelFrame(const RectI& rect, int bevelSize, Graphics& gfx) const
+{
+	DrawOuterBevel(rect, bevelSize, gfx);
+	DrawInnerBevel(rect.GetExpanded(-bevelSize), bevelSize, gfx);
 }
 
 void Beveler::SetBaseColor(Color baseColor_in)
